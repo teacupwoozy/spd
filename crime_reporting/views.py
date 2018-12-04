39e2a73d2fc111898a4a9732767426aaefceaf21
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-12-04 15:10:44 -0600

    add pseudo code for call_analysis context views

--- crime_reporting/views.py
+++ crime_reporting/views.py
@@ -26,10 +26,35 @@ class CallAnalysis(TemplateView):
         
         context = super(CallAnalysis, self).get_context_data(**kwargs)
         context['title'] = "Hallo. Call Analysis page"
+        
         # Calls that are reporting a sexual assault
         context['is_sexual_assault'] = calls.filter(is_sexual_assault=True)
         # Count of is_sexual_assault
         context['is_sexual_assault_count'] = context['is_sexual_assault'].count()
+
+        # Calls reporting lewd conduct
+        # Count of is_lewd_conduct
+
+        # Calls reporting molesting
+        # Count of is_molesting
+
+        # Calls reporting commercial sexual exploitation of minors (CSEC)
+        # Count of is_CSEC
+
+        # Calls reporting rape
+        # Count of is_rape
+
+        # Calls reporting an in-progress or just-occurred rape
+        # Count of ip_or_jo_rape
+
+        # Calls reporting an aquaintance rape
+        # Count of is_aquaintance_rape
+
+        # Calls reporting a stranger rape
+        # Count of is_stranger_rape
+
+        # Count of all_rapes
+
         return context
 
 
